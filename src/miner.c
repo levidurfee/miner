e147036ef90e5a4a15cf23ef6b70dcfeaf416585
levi durfee <levi.durfee@gmail.com>
2017-07-19 08:55:46 -0400

    include header file for constants

--- src/miner.c
+++ src/miner.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/sha.h>
+#include "miner.h"
 
 int get_hash(char input[MIN_SIZE], char output[MID_SIZE], size_t len) {
     unsigned char md[LOW_SIZE];         // message digest
